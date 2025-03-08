ad30d7ff81c73e054a0367e0c87f7fffadb1a256
AndrewPal04 <aapalacios@cpp.edu>
2025-03-07 23:41:02 -0800

    added subtract tests

--- test_calculator.c
+++ test_calculator.c
@@ -5,7 +5,7 @@ void setUp(void){
 
 }
 void tearDown(void){
-
+//add tests
 }
 void test_add_positive_numbers(void){
     TEST_ASSERT_EQUAL(5,add(2,3));
@@ -22,19 +22,50 @@ void test_add_zero(void){
 }
 void test_add_overflow(void){
     int result = add(INT_MAX,1);
-    TEST_ASSERT_TRUE(result>0);
+    TEST_ASSERT_TRUE(result<0);
 }
 void test_add_underflow(void){
     int result = add(INT_MIN,-1);
     TEST_ASSERT_TRUE(result>0);
 }
+//subtract tests
+void test_subtract_positive_numbers(void){
+    TEST_ASSERT_EQUAL(2, subtract(5,3));
+}
+void test_subtract_positive_and_negative_numbers(void){
+    TEST_ASSERT_EQUAL(7,subtract(5,-2));
+}
+void test_subtract_negative_numbers(void){
+    TEST_ASSERT_EQUAL(1,subtract(-2,-3));
+}
+void test_subtract_zero(void){
+    TEST_ASSERT_EQUAL(10,subtract(10,0));
+    TEST_ASSERT_EQUAL(0,subtract(0,0));
+}
+void test_subtract_overflow(void){
+    int result = subtract(INT_MAX,-1);
+    TEST_ASSERT_TRUE(result<0);
+}
+void test_subtract_underflow(void){
+    int result = subtract(INT_MIN, 1);
+    TEST_ASSERT_TRUE(result>0);
+}
+
 int main(void){
     UNITY_BEGIN();
+    //add tests
     RUN_TEST(test_add_positive_numbers);
     RUN_TEST(test_add_positive_and_negative_numbers);
     RUN_TEST(test_add_negative_numbers);
     RUN_TEST(test_add_zero);
     RUN_TEST(test_add_overflow);
     RUN_TEST(test_add_underflow);
+    //subtract tests
+    RUN_TEST(test_subtract_positive_numbers);
+    RUN_TEST(test_subtract_positive_and_negative_numbers);
+    RUN_TEST(test_subtract_negative_numbers);
+    RUN_TEST(test_subtract_zero);
+    RUN_TEST(test_subtract_overflow);
+    RUN_TEST(test_subtract_underflow);
     return UNITY_END();
 }
